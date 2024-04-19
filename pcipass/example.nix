3a8e81ef4ce8d63fc5a9e73c6a4897430e066ce2
nunoot <167473453+nunoot@users.noreply.github.com>
2024-04-19 18:28:34 +0000

    Update pcipass/example.nix

--- pcipass/example.nix
+++ pcipass/example.nix
@@ -5,16 +5,20 @@
     ./pci.nix
   ];
 
-  # Make sure to leave a way to access your host, for example, using the main configuration as the host, or creating an extra specialisation for the host.
-
   # Set this to an existing user of your choice.
   virtualisation.autoStart.user = "user";
+  # Set the ids of the PCI devices to be blacklisted from the host, namely your GPU.
+  passthrough.ids = "xxxx:aaaa,xxxx:bbbb";
 
   specialisation = {
     example.configuration.virtualisation = {
+      # Enabling virtualisation.passthrough.enable will blacklist your virtualisation.passthrough.ids from the host, allowing them to be used in a VM.
+      # Be careful not to enable this configuration wide, as that would blacklist your PCI devices (i.e, your GPU) for all boot targets, leaving you with no way to boot into your host's DE/WM.
       passthrough.enable = true;
-      # Set the ids of the PCI devices to be blacklisted from the host, namely your GPU.
-      passthrough.ids = "xxxx:aaaa,xxxx:bbbb";
+      # Enabling this will allow the system.userActivationScripts.autoStartVM script to run, starting the virtualisation.autoStart.vm VM.
+      # If you'd like to use a different method of starting your VM at boot (or manually start your VM), feel free to disable this.
+      autoStart.enable = true;
+
       # Set the name of the VM to be started when booting into this specialisation.
       autoStart.vm = "win10";
     };
